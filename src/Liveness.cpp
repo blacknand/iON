c8268c79bca108d56c9d8a60f92a831fa4774fe6
Nathan Blackburn <nblackburndeveloper@icloud.com>
2026-02-16 17:45:19 +0000

    refactor computeUseDef to handle instructions of generic form rather than x <- y op z

--- src/Liveness.cpp
+++ src/Liveness.cpp
@@ -18,28 +18,47 @@ LivenessInfo computeUseDef(Function& fn) {
             // Uses stores the registers used inside an instruction
             // where y = uses[0] and z = uses[1]
             // i.e., %3 = ADD %1, %2 = x <- y op z
-            if (uses.size() == 2 && 
-                std::holds_alternative<VReg>(uses[1]) && 
-                std::holds_alternative<VReg>(uses[2])) {
-                // If use is a VReg, check in set using index
-                VReg y = std::get<VReg>(uses[0]);
-                VReg z = std::get<VReg>(uses[1]);
+            // if (uses.size() == 2 && 
+            //     std::holds_alternative<VReg>(uses[1]) && 
+            //     std::holds_alternative<VReg>(uses[2])) {
+            //     // If use is a VReg, check in set using index
+            //     VReg y = std::get<VReg>(uses[0]);
+            //     VReg z = std::get<VReg>(uses[1]);
 
-                // If y NOT IN VarKill(block)
-                if (!li.VarKill[block->id][y.id])
-                    li.UEVar[block->id][y.id] = true;
+            //     // If y NOT IN VarKill(block)
+            //     if (!li.VarKill[block->id][y.id])
+            //         li.UEVar[block->id][y.id] = true;
 
-                // If z NOT IN VarKill(block)
-                if (!li.VarKill[block->id][z.id])
-                    li.UEVar[block->id][z.id] = true;
+            //     // If z NOT IN VarKill(block)
+            //     if (!li.VarKill[block->id][z.id])
+            //         li.UEVar[block->id][z.id] = true;
 
-                // Add x (operand) to VarKill unconditionally
-                // check not std::monostate first
-                if (std::holds_alternative<VReg>(def)) {
-                    VReg x = std::get<VReg>(def);
-                    li.VarKill[block->id][x.id] = true;
+            //     // Add x (operand) to VarKill unconditionally
+            //     // check not std::monostate first
+            //     if (std::holds_alternative<VReg>(def)) {
+            //         VReg x = std::get<VReg>(def);
+            //         li.VarKill[block->id][x.id] = true;
+            //     }
+
+            //     // TODO: still need to handle non-generic instructions
+            // }
+
+            for (const auto &var : uses) {
+                if (std::holds_alternative<VReg>(var)) {
+                    // If var NOT IN VarKill(block)
+                    VReg v = std::get<VReg>(var);
+                    if (!li.VarKill[block->id][v.id])
+                        li.UEVar[block->id][v.id] = true;
                 }
             }
+
+            // Add x (operand) to VarKill unconditionally
+            // check not std::monostate first
+            if (std::holds_alternative<VReg>(def)) {
+                VReg x = std::get<VReg>(def);
+                li.VarKill[block->id][x.id] = true;
+            }
+
         }
     }
     return li;
