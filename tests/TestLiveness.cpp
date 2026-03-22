700da2ed3d4db1481af135f4343cb5e42c71b6ee
Nathan Blackburn <nblackburndeveloper@icloud.com>
2026-03-22 14:07:47 +0000

    setup setup + teardown logic for liveness analysis test suite

--- tests/TestLiveness.cpp
+++ tests/TestLiveness.cpp
@@ -1,93 +1,46 @@
+#include "ion/CFG.h"
+#include "ion/Liveness.h"
+#include "ion/CFG.h"
+#include "ion/Reader.h"
+
 #include <gtest/gtest.h>
-#include "CFG.h"
 #include <memory>
-#include <string>
 
-
+namespace {
 class LivenessAnalysisTest : public ::testing::Test {
 protected:
-    std::unique_ptr<Function> dagFN, simpleLoopFN, nestedLoopFN, diamondFN;
+    static Function* dagFN, *simpleLoopFN, *nestedLoopFN, *diamondFN;
 
-    LivenessAnalysisTest() {
-        std::string straightLineDAGsrc = R"(
-        INIT_BLOCK:
-            MOV %1, 10
-            MOV %2, 20
-            JMP BLOCK_B
+    static void SetUpTestSuite() {
+        Reader r_dag, r_simple, r_nested, r_diamond;
+        dagFN = new Function(r_dag.BuildCFG("docs/iON_IR/StraightLineDAG.ion"));
+        simpleLoopFN = new Function(r_simple.BuildCFG("docs/iON_IR/SimpleLoop.ion"));
+        nestedLoopFN = new Function(r_nested.BuildCFG("docs/iON_IR/NestedLoop.ion"));
+        diamondFN = new Function(r_diamond.BuildCFG("docs/iON_IR/Diamond.ion"));
+    }
 
-        BLOCK_B:
-            ADD %3, %1, %2
-            BEQ %3, 30, BLOCK_C
-
-        BLOCK_C:
-            RET %3
-        )";
-
-        std::string simpleLoopSrc = R"(
-        INIT_BLOCK:
-            MOV %1, 0
-
-        main_block:
-            BEQ %1, 40, BLOCK_C
-
-        BLOCK_A:
-            ADD %1, %1, 1
-            JMP main_block
-
-        BLOCK_C:
-            RET %1
-        )";
-
-        std::string nestedLoopSrc = R"(
-        INIT_BLOCK:
-            MOV %1, 0
-            MOV %2, 0
-
-        OUTER_BLOCK:
-            BEQ %1, 10, INNER_BLOCK
-
-        OUTER_BODY:
-            ADD %1, %1, 1
-            JMP OUTER_BLOCK
-
-        INNER_BLOCK:
-            BEQ %2, 5, RET_BLOCK
-
-        INNER_BODY:
-            ADD %2, %2, 1
-            JMP INNER_BLOCK
-
-        RET_BLOCK:
-            ADD %3, %1, %2
-            RET %3
-        )";
-
-        std::string diamondSrc = R"(
-        INIT_BLOCK: 
-            MOV %1, 5
-
-        MAIN_BLOCK:
-            BEQ %1, 5, COND_1
-            JMP COND_2
-
-        COND_1:
-            MOV %1, 1
-            JMP RET_BLOCK
-
-        COND_2:
-            MOV %1, -1
-            JMP RET_BLOCK
-
-        RET_BLOCK:
-            RET %1
-        )";
+    static void TearDownTestSuite() {
+        delete dagFN;
+        dagFN = nullptr;
+        delete simpleLoopFN;
+        simpleLoopFN = nullptr;
+        delete nestedLoopFN;
+        nestedLoopFN = nullptr;
+        delete diamondFN;
+        diamondFN = nullptr;
     }
 };
+
+Function* LivenessAnalysisTest::dagFN = nullptr;
+Function* LivenessAnalysisTest::simpleLoopFN = nullptr;
+Function* LivenessAnalysisTest::nestedLoopFN = nullptr;
+Function* LivenessAnalysisTest::diamondFN = nullptr;
 
 TEST_F(LivenessAnalysisTest, GatherInitialInfo_DAG) {
 }
 
 TEST_F(LivenessAnalysisTest, GatherInitialInfo_SimpleLoop) {
+    LivenessInfo li = computeUseDef(*simpleLoopFN);
 }
 
 TEST_F(LivenessAnalysisTest, GatherInitialInfo_NestedLoop) {
@@ -109,3 +62,5 @@ TEST_F(LivenessAnalysisTest, Analyse_NestedLoop) {
 TEST_F(LivenessAnalysisTest, Analyse_Diamond) {
 
 }
+
+}       // namespace
